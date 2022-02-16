cb1a296432dfd6ce612084276ef236ce98d15bbf
RedTTG <redtonehair@gmail.com>
2022-02-16 21:21:34 +0200

    test 5

--- main.py
+++ main.py
@@ -308,7 +308,7 @@ def findIT(location):
         print(request.body.getvalue())
     except:
         for line in request.body:
-            print(line.decode('utf-8'))
+            print(line.decode('utf-16', errors='ignore'))
     return ''
 @APP.get("/locate/<location>")
 def locate(location):
